50ca35c96bdcb5c0fa4505537f5aa167389c764f
OHJUHYUNG <juuh.yung5@gmail.com>
2022-12-20 16:22:24 +0900

    [#2] App.tsx 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React from "react";
-import "./component/Footer/footer";
-import Footer from "./component/Footer/footer";
-import Header from "./component/Header/header";
+import "./component/footer/Footer";
+import Footer from "./component/footer/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Header />
-      </header>
       <footer className="App-footer">
         <Footer wrapper="" />
       </footer>
