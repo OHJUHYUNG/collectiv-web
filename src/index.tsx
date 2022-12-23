92c1ae1c57c320d4e8f2bdf8e47a86fe549643ac
leodioKim <kevin8603k@gmail.com>
2022-12-23 12:27:36 +0900

    coflict

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import ReactDom from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import './index.css';
+import "./index.css";
 
 ReactDom.render(
   <BrowserRouter>
